75545530b76599565dc695cd758da3791bb6b095
babi-barbato <barbara.oliveira@sptech.school>
2026-02-25 21:07:48 -0300

    feat: Aula 3

---
+++ aula3/extrairPdfComImagemTexto.py
@@ -0,0 +1,26 @@
+import pytesseract
+from PIL import Image
+from pdf2image import convert_from_path
+
+poppler_path = r"C:\poppler-26.02.0"
+
+def extrect_text_from_pdf(pdf_path):
+    # Converter o PDF em uma lista de imagens
+    imagens = convert_from_path(pdf_path, poppler_path=poppler_path)
+
+    texto_extraido = ""
+
+    # Iterar sobre cada imagem e extrair usando OCR 
+    for i, imagem in enumerate(imagens):
+        texto_imagem =pytesseract.image_to_string(imagem, lang='por')
+        texto_extraido += f"Texto da pagina {i+1};\n{texto_imagem}\n\n"
+
+    return texto_extraido
+
+# Caminho da Imagem
+image_path = ""
+
+# Realizar OCR na imagem
+ocr_text = extrect_text_from_pdf(image_path)
+print("Texto extraído do PDF:")
+print(ocr_text)
---
+++ aula3/extrairTextodePdf.py
@@ -0,0 +1,14 @@
+import fitz
+
+doc = fitz.open(r"C:\Users\babib\OneDrive\Documentos\paradigmas-programacao\aula3\pdfComImagem.pdf-")
+page = doc[0]
+text = page.get_text()
+
+if text.strip():
+    print("PDF possui texto digital")
+    print(text)
+else:
+    print("PDF provavelmente é escaneado")
+
+# FOCAR EM extrair texto que Não tem algo ou tem
+# Texto OCR para esse texto
