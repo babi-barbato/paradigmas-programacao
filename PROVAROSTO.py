05d7fc1b4f8ce395b021ff5904ad5406b8e43f9a
babi-barbato <barbara.oliveira@sptech.school>
2026-04-15 12:41:10 -0300

    feat: Estudo para a prova quest

---
+++ PROVAROSTO.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# Caminho da imagem
+pathImg = r'C:/Users/babib/OneDrive/Documentos/paradigmas-programacao/sorriso2.jpg'
+
+# Carregar imagem
+imagem = cv2.imread(pathImg)
+
+# Converter para escala de cinza
+imagemGray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+# Carregar classificadores
+face_xml = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+smile_xml = cv2.data.haarcascades + 'haarcascade_smile.xml'
+
+face_cascade = cv2.CascadeClassifier(face_xml)
+smile_cascade = cv2.CascadeClassifier(smile_xml)
+
+# Detectar rostos
+faces = face_cascade.detectMultiScale(
+    imagemGray,
+    scaleFactor=1.2,
+    minNeighbors=2,
+    minSize=(60, 60)
+)
+
+total_sorrisos = 0
+
+# Para cada rosto encontrado
+for (x, y, w, h) in faces:
+    # Desenhar retângulo no rosto
+    cv2.rectangle(imagem, (x, y), (x+w, y+h), (255, 0, 0), 5)
+
+    roi_gray = imagemGray[y:y+h, x:x+w]
+    roi_color = imagem[y:y+h, x:x+w]
+
+    # PEGAR APENAS PARTE INFERIOR DO ROSTO (onde fica o sorriso)
+    roi_gray_lower = roi_gray[int(h/2):h, :]
+    roi_color_lower = roi_color[int(h/2):h, :]
+
+    # Detectar sorriso
+    smiles = smile_cascade.detectMultiScale(
+        roi_gray_lower,
+        scaleFactor=1.7,
+        minNeighbors=17,
+        minSize=(25, 25)
+    )
+
+    total_sorrisos += len(smiles)
+
+    # Desenhar retângulos nos sorrisos
+    for (sx, sy, sw, sh) in smiles:
+        cv2.rectangle(roi_color_lower, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 5)
+
+print('Sorrisos encontrados:', total_sorrisos)
+
+# Mostrar imagem
+plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
---
+++ sprint2/aula5/identificarRostoValid.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# Vai cair smile na prova SMILE
+
+pathImg = r'C:\Users\babib\OneDrive\Documentos\paradigmas-programacao\sorriso2.jpg'
+# pathImg2 = r'C:\Users\babib\OneDrive\Documentos\paradigmas-programacao\sprint2\aula5\palmeiras.jpg'     1.3   60,60
+path1 = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+
+imagem = cv2.imread(pathImg)
+imagemGray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+face_frontal = cv2.CascadeClassifier(path1)
+face_recon = face_frontal.detectMultiScale(
+    imagemGray,
+    scaleFactor=1.2,
+    minNeighbors=2,
+    minSize=(60, 60)
+)
+
+
+# ()
+print('Faces Encontradas: ', len(face_recon))
+
+for (x, y, w, h) in face_recon:
+    cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4)
+
+
+plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
